ed55041577e70fab4b65f32460ed4245092609e6
Tudor Gheorghiu <tudi.gheorghiu@gmail.com>
2024-12-05 14:24:44 +0200

    Finished CRUD operations on the /task endpoint

--- back/src/models/task/task.model.js
+++ back/src/models/task/task.model.js
@@ -2,7 +2,7 @@ import {Task} from '../index.js';
 import connection from '../../services/mysql_connection.js';
 
 const taskPriorities = [1, 2, 3, 4, 5];
-const taskStatus = ['Completed', 'In Progress', 'Not Started', 'Pending'];
+const taskStatus = ['Completed', 'In Progress', 'Not Started', 'Pending Review'];
 
 async function getAllTasks(ORM = true) {
     if (!ORM) {
@@ -44,6 +44,37 @@ async function addTask(task, ORM = true) {
     }
 }
 
+async function updateTask(task, ORM = true) {
+    if (!ORM) {
+        connection.query('UPDATE TASKS SET ? WHERE id = ?', task, task.id, (err, results, fields) => {
+            if (err) throw err;
+            console.log(results.insertId);
+        });
+    }
+    else {
+        return await Task.update(task, {
+            where: { id: task.id }
+        });
+    }
+}
+
+async function deleteTask(id, ORM = true) {
+    if (!ORM) {
+        connection.query('DELETE FROM TASKS WHERE id = ?', id, (err, results, fields) => {
+            if (err) throw err;
+            console.log(results.insertId);
+        });
+    }
+    else {
+        return await Task.destroy({
+            where: { id: id }
+        });
+    }
+}
+
+async function findTaskByPk(taskId) {
+    return await Task.findByPk(taskId);
+}
 
 export {
     getAllTasks,
@@ -51,4 +82,7 @@ export {
     addTask,
     taskPriorities,
     taskStatus,
+    findTaskByPk,
+    updateTask,
+    deleteTask
 }
