97e8f861ccff863a631c79827e5d6a93f847861f
Tudor Gheorghiu <tudi.gheorghiu@gmail.com>
2024-12-06 17:29:31 +0200

    Modified the db config code, because we migrated from local mariadb server, to remote cloud db hosting Railway. Also created npm run script for db schema syncing

--- back/src/config/db.js
+++ back/src/config/db.js
@@ -4,17 +4,31 @@ import {flatten} from "express/lib/utils.js";
 
 dotenv.config();
 
-const sequelize = new Sequelize(
-    process.env.DB_DATABASE,
-    process.env.DB_USERNAME,
-    process.env.DB_PASSWORD,
-    {
-        host: process.env.DB_HOST,
-        dialect: process.env.DB_DIALECT,
-        logging: false
-    }
-);
+// const sequelize = new Sequelize(
+//     process.env.DB_DATABASE,
+//     process.env.DB_USERNAME,
+//     process.env.DB_PASSWORD,
+//     {
+//         host: process.env.DB_HOST,
+//         dialect: process.env.DB_DIALECT,
+//         logging: false
+//     }
+// );
 
+const sequelize = new Sequelize(process.env.MYSQL_URL, {
+    dialect: 'mysql',
+    dialectOptions: {
+        ssl: {
+            rejectUnauthorized: false
+        }
+    },
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+    }
+});
 
 (async () => {
     try {
--- back/src/server.js
+++ back/src/server.js
@@ -1,14 +1,11 @@
 import http from 'http';
 import app from './app.js';
-import syncDB from "./services/sequelize.js";
 
 const PORT = process.env.PORT || 8000;
 
 const server = http.createServer(app);
 
 async function startServer() {
-    // await syncDB();
-
     server.listen(PORT, () => {
         console.log("Server listening on PORT", PORT);
     })
