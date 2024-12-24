0711a46c9a87e92b05f6f0dcd2be9ac7b89c9a75
Tudor Gheorghiu <tudi.gheorghiu@gmail.com>
2024-12-24 13:31:35 +0200

    Removed all direct MySQL connection code and kept only the Sequelize ORM code to fix remote db connection conflicts

--- back/src/models/user/user.model.js
+++ back/src/models/user/user.model.js
@@ -1,51 +1,21 @@
-import {User} from '../index.js';
-import connection from "../../services/mysql_connection.js";
+import { User } from '../index.js';
 
-async function getAllUsers(ORM = true) {
-    if (!ORM) {
-        connection.query('SELECT * FROM USERS', (err, results, fields) => {
-            if (err) throw err;
-            console.log(results.insertId);
-        });
-    }
-    else {
-        return await User.findAll({});
-    }
-}
-
-async function addUser(user, ORM = true) {
+async function addUser(user) {
     const newUser = Object.assign(user, {
         isAdmin: false,
         profilePicture: null
     });
-
-    if (!ORM) {
-        connection.query('INSERT INTO USERS SET ?', newUser, (err, results, fields) => {
-            if (err) throw err;
-            console.log(results.insertId);
-        });
-    }
-    else {
-        return await User.create(newUser);
-    }
+    return await User.create(newUser);
 }
 
 async function getUsers() {
     return await User.findAll({});
 }
 
-async function updateUser(user, ORM = true) {
-    if (!ORM) {
-        connection.query('UPDATE TOKENS SET ? WHERE email = ?', user, user.email, (err, results, fields) => {
-            if (err) throw err;
-            console.log(results.insertId);
-        });
-    }
-    else {
-        return await User.update(user, {
-            where: { email: user.email }
-        });
-    }
+async function updateUser(user) {
+    return await User.update(user, {
+        where: { email: user.email }
+    });
 }
 
 async function checkIfUserExists(email) {
